db3f54a1f672a7f1738737627162700aa3f72860
Anz <50573902+Anz1x@users.noreply.github.com>
2022-08-20 19:10:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 import pandas as pd
+import colorama
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from datetime import datetime
+from colorama import Fore
+
+colorama.init(autoreset=True)
+
+now = datetime.now()
+thedate = now.strftime("-%m_%d_%Y")
 
 url = "https://www.thesun.co.uk/sport/football/"
 path = "chromedriver.exe"
 
+options = Options()
+options.headless = True
+
 service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 driver.get(url)
 
 containers = driver.find_elements(by="xpath", value="//div[@class='teaser__copy-container']")
@@ -31,7 +44,11 @@ csv_dict = {"Title":titles,
            "Subtitle":subtitles,
            "Link":links}
 
+file = f"ftbl_headlines{thedate}.csv"
+
 df_headline = pd.DataFrame(csv_dict)
-df_headline.to_csv("ftbl_headlines.csv")
+df_headline.to_csv(file)
+
+print(f"{Fore.LIGHTGREEN_EX}[+] {Fore.WHITE}Successfully extracted the news source! Outputted the data in {Fore.LIGHTGREEN_EX + file}")
 
 driver.quit()
